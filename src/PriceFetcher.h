af6deef5352456ee1120ac448e32834dfe1b538e
0n1cOn3 <0n1cOn3@gmx.ch>
2025-06-05 08:06:55 +0200

    Add full log view for issues

--- src/PriceFetcher.h
+++ src/PriceFetcher.h
@@ -5,39 +5,60 @@
 #include <QList>
 #include <QUrl>
 #include <QRegularExpression>
+#include <QDate>
 
 struct PriceEntry {
     QString store;
     QString item;
     QDate date;
     double price;
+    QString currency;
 };
+
+struct IssueEntry {
+    QString store;
+    QString item;
+    QDate date;
+    QString error;
+};
+
+class DatabaseManager;
 
 class PriceFetcher : public QObject
 {
     Q_OBJECT
 public:
-    explicit PriceFetcher(QObject *parent = nullptr);
+    explicit PriceFetcher(DatabaseManager *db, QObject *parent = nullptr);
     void fetchDailyPrices();
     QStringList storeList() const;
     QStringList categoryList() const;
+    void addItem(const QString &item);
 
 signals:
     void priceFetched(const PriceEntry &entry);
+    void issueOccurred(const IssueEntry &issue);
     void fetchFinished();
     void fetchStarted();
+    void progressChanged(int done, int total);
+    void itemListChanged();
 
 private slots:
     void onReply(QNetworkReply *reply);
 
 private:
-    struct StoreProduct {
+    enum class RequestStage { Search, Product };
+
+    struct StoreInfo {
         QString store;
-        QString item;
-        QString url;
-        QRegularExpression priceRegex;
+        QString searchUrl;          // use %1 for item
+        QRegularExpression productRegex; // extracts product URL from search page
+        QRegularExpression priceRegex;   // extracts price from product page
     };
-    QList<StoreProduct> m_products;
+
+    QList<StoreInfo> m_stores;
+    QStringList m_items;
     int m_pending = 0;
+    int m_total = 0;
     QNetworkAccessManager m_manager;
+    DatabaseManager *m_db = nullptr;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -2,27 +2,72 @@
 #include "PriceFetcher.h"
 #include "DatabaseManager.h"
 #include "PlotWindow.h"
+#include "FirstRunDialog.h"
+#include <QEventLoop>
+
+static bool performFirstScrape(PriceFetcher &fetcher, DatabaseManager &db)
+{
+    FirstRunDialog dialog;
+    QObject::connect(&fetcher, &PriceFetcher::progressChanged,
+                     &dialog, &FirstRunDialog::onProgress);
+    QObject::connect(&fetcher, &PriceFetcher::issueOccurred,
+                     &dialog, &FirstRunDialog::onIssue);
+    QObject::connect(&fetcher, &PriceFetcher::fetchStarted,
+                     &dialog, &FirstRunDialog::onFetchStarted);
+
+    bool canceled = false;
+    QObject::connect(&dialog, &FirstRunDialog::canceled, [&](){ canceled = true; });
+    dialog.show();
+
+    while (!db.hasPrices() && !canceled) {
+        QEventLoop loop;
+        QObject::connect(&fetcher, &PriceFetcher::fetchFinished,
+                         &loop, &QEventLoop::quit);
+        fetcher.fetchDailyPrices();
+        loop.exec();
+    }
+
+    dialog.hide();
+    return db.hasPrices() && !canceled;
+}
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+    QApplication::setApplicationName("Foodcoop");
 
     DatabaseManager db;
     db.open("prices.db");
 
     PlotWindow w(&db);
-    w.show();
 
-    PriceFetcher fetcher;
+    PriceFetcher fetcher(&db);
     w.setStoreList(fetcher.storeList());
     w.setCategoryList(fetcher.categoryList());
     QObject::connect(&fetcher, &PriceFetcher::priceFetched,
                      [&db](const PriceEntry &entry){ db.insertPrice(entry); });
+    QObject::connect(&fetcher, &PriceFetcher::issueOccurred,
+                     [&db, &w](const IssueEntry &issue){
+                         db.insertIssue(issue);
+                         w.onIssueOccurred(issue);
+                     });
     QObject::connect(&fetcher, &PriceFetcher::fetchStarted,
                      &w, &PlotWindow::onFetchStarted);
     QObject::connect(&fetcher, &PriceFetcher::fetchFinished,
                      &w, &PlotWindow::onFetchFinished);
-    fetcher.fetchDailyPrices();
+    QObject::connect(&w, &PlotWindow::addItemRequested,
+                     &fetcher, &PriceFetcher::addItem);
+    QObject::connect(&fetcher, &PriceFetcher::itemListChanged,
+                     [&w, &fetcher]() {
+                         w.setCategoryList(fetcher.categoryList());
+                         w.updateChart();
+                     });
+    if (!db.hasPrices()) {
+        if (!performFirstScrape(fetcher, db))
+            return 0;
+    }
+
+    w.show();
 
     return app.exec();
 }
